ab4a42edee698bf248b30869856d6c41c237e8f2
Rio Aulia Yahya <rioaulia23@gmail.com>
2025-02-28 16:17:04 +0700

    refactor: remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'navigation/bottom_nav.dart'; // Import the bottom navigation
+import 'navigation/bottom_nav.dart';
 
 Future<void> main() async {
   await initialize(); // Initialize app settings
